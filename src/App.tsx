226dcefd23e63e559b35200367a0337ca9830fc1
Julian Maldonado <jmaldonado@kindalab.co>
2023-11-28 16:36:25 -0300

    add filter and filter item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Greeting from "./components";
+import Filter from "./components/Filter";
 
 const App = () => {
-  return (
-    <>
-      <h1>REACT FILTERS URL BUILDER</h1>
-      <Greeting message="URL BUILDER" />
-    </>
-  );
+	return (
+		<>
+			<h1>REACT FILTERS URL BUILDER</h1>
+			<Filter>
+				<Filter.Item title="Filter Item" onClick={() => console.log("Click")} />
+			</Filter>
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,31 @@
+import { FC, PropsWithChildren } from "react";
+import cn from "../../utils/style";
+import FilterItem from "../FilterItem";
+import { FilterItemProps } from "../FilterItem/FilterItem";
+
+type FilterProps = PropsWithChildren & {
+	className?: string;
+};
+
+interface SubComponentsProps {
+	Item: FC<FilterItemProps>;
+}
+
+const Filter: FC<FilterProps> & SubComponentsProps = ({
+	children,
+	className,
+}) => {
+	return (
+		<div
+			className={cn(
+				"w-screen max-w-md flex-auto overflow-hidden rounded-xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5",
+				className,
+			)}>
+			{children}
+		</div>
+	);
+};
+
+Filter.Item = FilterItem;
+
+export default Filter;
